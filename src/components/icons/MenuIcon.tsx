e265f985c1fb6d4b1da54be78790707841ac5cba
Leon Delaimy <leondel.coding@gmail.com>
2023-01-16 21:32:34 +0000

    icons: update mobile menu icons

---
+++ src/components/icons/MenuIcon.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const MenuIcon: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12" version="1.1">
+    <rect height="0.5" width="10" x="0.5" y="5.5"></rect>
+    <rect height="0.5" width="10" x="0.5" y="2.5"></rect>
+  </svg>
+)
