6ade4d05371316daf8e25c635f5b057c4d3deda8
Leon Delaimy <leondel.coding@gmail.com>
2023-01-17 09:05:59 +0000

    about: refactor section

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,36 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Title } from '@components'
-import { Trail } from '@animations'
-import { StyledButton } from '@styles'
-import scrollTo from 'gatsby-plugin-smoothscroll'
 
 const MyNameIs = styled(Title)`
   color: ${({ theme }) => theme.colors.zimaBlue};
   padding: 10px;
-  margin: 3rem 0 0 0;
+  margin: 0;
 `
 
 const Name = styled(Title)`
-  padding: 5px;
+  padding: 10px;
   margin: 0;
-
-  @media only screen and (max-width: 992px) {
-    padding: 8px;
-  }
 `
 
 const StyledHeader = styled.header`
   display: flex;
   justify-content: center;
   flex-direction: column;
-  margin: 0 auto;
-  min-height: 100vh;
-
-  .btn-wrapper {
-    display: flex;
-    justify-content: center;
-  }
+  padding-bottom: 10px;
+  gap: 5rem;
 
   p {
     padding: 10px;
@@ -44,33 +32,10 @@ const StyledHeader = styled.header`
 export const Header: React.FC = () => {
   return (
     <StyledHeader id="intro">
-      <Trail open={true}>
-        <div className="text">
-          <MyNameIs headingLevel="h5">Hey, my name is</MyNameIs>
-          <Name headingLevel="h2">Leon Delaimy</Name>
-          <p>I&apos;m a software engineer with full stack experience</p>
-        </div>
-        <FindOutMoreBtn />
-      </Trail>
-    </StyledHeader>
-  )
-}
-
-const FindOutMoreBtn = () => {
-  return (
-    <StyledButton primary>
-      <div className="btn-wrapper">
-        <ul>
-          <li
-            tabIndex={0}
-            onClick={() => scrollTo('#about')}
-            onKeyDown={() => null}
-            role="button" // eslint-disable-line jsx-a11y/no-noninteractive-element-to-interactive-role
-          >
-            Find out more
-          </li>
-        </ul>
+      <div className="text">
+        <MyNameIs headingLevel="h5">Hey, my name is</MyNameIs>
+        <Name headingLevel="h2">Leon Delaimy</Name>
       </div>
-    </StyledButton>
+    </StyledHeader>
   )
 }
--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Title, Image } from '@components'
-
-const AboutTitle = styled(Title)`
-  color: ${({ theme }) => theme.colors.zimaBlue};
-  padding: 0 30px 0 30px;
-  margin: 15px auto;
-`
+import { Header } from '@components'
+import { useStaticQuery, graphql } from 'gatsby'
+import { GatsbyImage, getImage, IGatsbyImageData } from 'gatsby-plugin-image'
 
 const StyledAbout = styled.section`
   display: flex;
   justify-content: center;
   flex-direction: column;
-  margin: 0 auto;
   min-height: 100vh;
 
   .about-wrapper {
@@ -21,6 +16,7 @@ const StyledAbout = styled.section`
     justify-content: center;
     align-items: center;
     gap: 5rem;
+    padding: 10px;
   }
 
   a {
@@ -37,31 +33,67 @@ const StyledAbout = styled.section`
   }
 
   p {
-    max-width: 300px;
+    max-width: 400px;
     padding: 0 30px 0 30px;
     line-height: 1.8;
   }
 `
 
+const StyledImage = styled.div`
+  .img {
+    border: 30px solid ${({ theme }) => theme.colors.pastelRed};
+    border-left: 5px solid ${({ theme }) => theme.colors.pastelRed};
+    border-right: 5px solid ${({ theme }) => theme.colors.pastelRed};
+    border-bottom: 15px solid ${({ theme }) => theme.colors.pastelRed};
+    clip-path: polygon(
+      0px 25px,
+      26px 0px,
+      calc(60% - 25px) 0px,
+      60% 25px,
+      100% 25px,
+      100% calc(100% - 10px),
+      calc(100% - 15px) calc(100% - 10px),
+      calc(80% - 10px) calc(100% - 10px),
+      calc(80% - 15px) calc(100% - 0px),
+      10px calc(100% - 0px),
+      0% calc(100% - 10px)
+    );
+    background-color: ${({ theme }) => theme.background};
+  }
+`
+
+const query = graphql`
+  query {
+    fileName: file(relativePath: { eq: "me.png" }) {
+      childImageSharp {
+        gatsbyImageData(width: 300, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+      }
+    }
+  }
+`
+
 export const About: React.FC = () => {
+  const data = useStaticQuery(query)
+  const image = getImage(data.fileName) as IGatsbyImageData
   return (
-    <StyledAbout id="about">
-      <div className="about-wrapper">
-        <div>
-          <AboutTitle headingLevel="h4">About me</AboutTitle>
-          <p>
-            Software engineer based in Manchester UK, passionate about{' '}
-            <a className="music" href="https://leraq.net" target="_blank" rel="noreferrer">
-              music
-            </a>
-            , science and tech. I&apos;m currently working as a consultant with{' '}
-            <a href="https://www.infinityworks.com" target="_blank" rel="noreferrer">
-              Infinity Works
-            </a>
-          </p>
+    <>
+      <StyledAbout id="about">
+        <div className="about-wrapper">
+          <div>
+            <Header />
+            <p>
+              I&apos;m a software engineer & artist with an interest in science, audio-visuals & generative art.
+              I&apos;m currently working as a consultant with{' '}
+              <a href="https://www.infinityworks.com" target="_blank" rel="noreferrer">
+                Infinity Works
+              </a>
+            </p>
+          </div>
+          <StyledImage>
+            <GatsbyImage image={image} className="img" alt="leon-delaimy" />
+          </StyledImage>
         </div>
-        <Image />
-      </div>
-    </StyledAbout>
+      </StyledAbout>
+    </>
   )
 }
