60370b1a9232362384c757dfd28ad816e3153e70
Leon Delaimy <leondel.coding@gmail.com>
2023-01-17 08:56:53 +0000

    page index: add listener for page width

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,33 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { PageProps } from 'gatsby'
-import { Layout, Header, Nav, About, Experience, Contact, Footer } from '@components'
+import { Layout, Nav, About, Experience, Projects, Contact, Footer } from '@components'
+import { Trail } from '@animations'
 
 const IndexPage: React.FC<PageProps> = () => {
-  const [menu, setMenuOpen] = useState(false)
+  const isBrowser = typeof window !== 'undefined'
+  const [width, setWidth] = useState<number>(isBrowser ? window.innerWidth : 0)
+
+  const handleWindowSizeChange = () => {
+    setWidth(window.innerWidth)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowSizeChange)
+    return () => {
+      window.removeEventListener('resize', handleWindowSizeChange)
+    }
+  }, [])
+
+  const isMobile = width <= 843
+  const [menu, setMenuOpen] = useState(isMobile ? false : true)
 
   return (
     <Layout>
-      <Header />
-      <Nav menu={menu} setMenuOpen={setMenuOpen} />
-      <About />
+      <Nav menu={menu} setMenuOpen={setMenuOpen} isMobile={isMobile} />
+      <Trail open={true}>
+        <About />
+      </Trail>
+      <Projects />
       <Experience />
       <Contact />
       <Footer />
