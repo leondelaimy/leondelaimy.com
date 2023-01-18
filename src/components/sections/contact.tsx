1905106a4d69a374465e3b5b82a12920195f26b7
Leon Delaimy <leondel.coding@gmail.com>
2023-01-18 10:42:36 +0000

    contact: update text & styles

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -6,20 +6,20 @@ import { Title } from '@components'
 const ContactTitle = styled(Title)`
   color: ${({ theme }) => theme.colors.zimaBlue};
   margin: 3rem 0 0 0;
+  padding: 0 30px 0 30px;
 `
 
 const StyledContact = styled.section`
   display: flex;
   justify-content: center;
   flex-direction: column;
-  align-items: center;
   margin: 0 auto;
   min-height: calc(100vh - 113px);
 
   p {
     padding: 0 30px 0 30px;
-    max-width: 300px;
-    text-align: center;
+    max-width: 400px;
+    line-height: 1.8;
   }
 
   .btn-wrapper {
@@ -32,7 +32,10 @@ export const Contact: React.FC = () => {
   return (
     <StyledContact id="contact">
       <ContactTitle headingLevel="h4">Contact me</ContactTitle>
-      <p>Have any questions and want to contact me? Ping me an email</p>
+      <p>
+        Full stack engineering experience with a degree in Biomedical Sciences. If you would like to contact me, ping me
+        an email
+      </p>
       <CVBtn show={true} />
     </StyledContact>
   )
