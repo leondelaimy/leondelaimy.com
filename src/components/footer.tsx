741ba3b4fc36728d337a38f65a45fafef9d4e4dc
Leon Delaimy <leondel.coding@gmail.com>
2023-09-03 17:40:52 +0100

    footer: add dots link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
-import { GitHubIcon, LinkedInIcon } from '@components'
+import { GitHubIcon, LinkedInIcon, TheDotsIcon } from '@components'
 
 const StyledFooter = styled.footer`
   margin: 0 auto;
+  text-align: center;
 
   .icons {
     display: flex;
@@ -30,6 +31,11 @@ export const Footer: React.FC = () => {
         <StyledIcon>
           <a href="https://github.com/leondelaimy" target="_blank" rel="noreferrer">
             <GitHubIcon />
+          </a>
+        </StyledIcon>
+        <StyledIcon>
+          <a href="https://the-dots.com/users/leon-delaimy-1481759" target="_blank" rel="noreferrer">
+            <TheDotsIcon />
           </a>
         </StyledIcon>
         <StyledIcon>
---
+++ src/components/icons/TheDotsIcon.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const TheDotsIcon: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <title>The Dots</title>
+    <path d="M11.9 0C5.4 0 0 5.4 0 11.9s5.4 11.9 11.9 11.9 11.9-5.4 11.9-11.9C23.9 5.4 18.5 0 11.9 0zm0 22.4c-5.8 0-10.5-4.7-10.5-10.5S6.2 1.5 11.9 1.5 22.4 6.2 22.4 12s-4.7 10.4-10.5 10.4z"></path>
+    <circle cx="11.9" cy="11.9" r="1.5"></circle>
+    <circle cx="11.9" cy="7.6" r="1.5"></circle>
+    <circle cx="7.6" cy="11.9" r="1.5"></circle>
+    <circle cx="16.3" cy="11.9" r="1.5"></circle>
+    <circle cx="11.9" cy="16.3" r="1.5"></circle>
+  </svg>
+)
